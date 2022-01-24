2a49459d1826251e7eee3072584f465b50a6fb60
Yutaka-Hidaka <97279402+Yutaka-Hidaka@users.noreply.github.com>
2022-01-24 19:19:41 +0900

    Update team.rb

--- team.rb
+++ team.rb
@@ -1,33 +1,33 @@
 #チームクラスの定義
 class Team
-   attr_accessor :name, :win, :lose, :draw
+  
       #initialize の処理を定義
     def initialize(name_hikisu, win_hikisu, lose_hikisu, draw_hikisu)
-       self.name = name_hikisu
-       self.win = win_hikisu
-       self.lose = lose_hikisu
-       self.draw = draw_hikisu
+       @name = name_hikisu
+       @win = win_hikisu
+       @lose = lose_hikisu
+       @draw = draw_hikisu
     end
      
          def calc_win_rate 
-            rate = self.win.to_f / (self.win + self.lose) 
+            rate = @win.to_f / (@win + @lose) 
             return rate
          end
-    def show_team_result #チームの成績をターミナルに表示する
-       puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は #{self.calc_win_rate}です。"
-    end
+      def show_team_result #チームの成績をターミナルに表示する
+       puts "#{@name}の2020年の成績は#{@win}勝#{@lose}敗#{@draw}分、勝率は #{self.calc_win_rate}です。"
+      end
 end
-Team_A=Team.new('Giants', 67, 45, 8)
-Team_B=Team.new('Tigers', 60, 53, 7)
-Team_C=Team.new('Dragons', 60, 55, 5)
-Team_D=Team.new('BayStars', 56, 58, 6)
-Team_E=Team.new('Carp', 52, 56, 12)
-Team_F=Team.new('Swallow', 41, 69, 10)
+team_A=Team.new('Giants', 67, 45, 8)
+team_B=Team.new('Tigers', 60, 53, 7)
+team_C=Team.new('Dragons', 60, 55, 5)
+team_D=Team.new('BayStars', 56, 58, 6)
+team_E=Team.new('Carp', 52, 56, 12)
+team_F=Team.new('Swallow', 41, 69, 10)
 
- Team_A.show_team_result
- Team_B.show_team_result
- Team_C.show_team_result
- Team_D.show_team_result
- Team_E.show_team_result
- Team_F.show_team_result
+ team_A.show_team_result
+ team_B.show_team_result
+ team_C.show_team_result
+ team_D.show_team_result
+ team_E.show_team_result
+ team_F.show_team_result
 
